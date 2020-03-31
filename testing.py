e46b1acaca3b302f551b3ee31d5e291713f73c14
marcalv <marcalv@protonmail.com>
2020-03-31 19:14:05 +0200

    test and fix sorting

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
-from rw import getData
+from read import getData
 from sorting import getPieceOrderBy
+import time
 
+start_time = time.time()
 
 # txt file to open
 dataFileExample = "ejemplar_calibrado_1.txt"
@@ -8,11 +10,14 @@ dataFileExample = "ejemplar_calibrado_1.txt"
 data = getData(dataFileExample)
 
 # Get piece order by TOTAL_PIECE_DURATION or WORKING_COST_AVERAGE ascending or descending
-pieceOrder1 = getPieceOrderBy(data, "TOTAL_PIECE_DURATION", True)
-pieceOrder2 = getPieceOrderBy(data, "TOTAL_PIECE_DURATION", False)
-pieceOrder3 = getPieceOrderBy(data, "WORKING_COST_AVERAGE", True)
-pieceOrder4 = getPieceOrderBy(data, "WORKING_COST_AVERAGE", False)
+for i in range(1,1000):
+    pieceOrder1 = getPieceOrderBy(data, "TOTAL_PIECE_DURATION", True)
+    pieceOrder2 = getPieceOrderBy(data, "TOTAL_PIECE_DURATION", False)
+    pieceOrder3 = getPieceOrderBy(data, "WORKING_COST_AVERAGE", True)
+    pieceOrder4 = getPieceOrderBy(data, "WORKING_COST_AVERAGE", False)
 print(pieceOrder1)
 print(pieceOrder2)
 print(pieceOrder3)
 print(pieceOrder4)
+
+print("--- %s seconds ---" % (time.time() - start_time))
